985130ef3fcf9ac40083b5b068d2e3a395de6a45
Anne <aaodeh@mun.ca>
2020-07-15 23:23:40 -0230

    Exercise 2.11

--- Work/report.py
+++ Work/report.py
@@ -3,10 +3,10 @@
 import csv
 
 def read_portfolio(filename):
-    
-    #Read a stock portfolio file into a list of dictionaries with keys
-    #name, shares, and price.
-    
+    '''
+    Read a stock portfolio file into a list of dictionaries with keys
+    name, shares, and price.
+    '''
     portfolio = []
     with open(filename) as f:
         rows = csv.reader(f)
@@ -23,9 +23,9 @@ def read_portfolio(filename):
     return portfolio
 
 def read_prices(filename):
-    
-    #Read a CSV file of price data into a dict mapping names to prices.
-    
+    '''
+    Read a CSV file of price data into a dict mapping names to prices.
+    '''
     prices = {}
     with open(filename) as f:
         rows = csv.reader(f)
@@ -37,22 +37,31 @@ def read_prices(filename):
 
     return prices
 
+def make_report_data(portfolio, prices):
+    '''
+    Make a list of (name, shares, price, change) tuples given a portfolio list
+    and prices dictionary.
+    '''
+    rows = []
+    for stock in portfolio:
+        current_price = prices[stock['name']]
+        change = current_price - stock['price']
+        summary = (stock['name'], stock['shares'], current_price, change)
+        rows.append(summary)
+    return rows
+        
+# Read data files and create the report data        
+
 portfolio = read_portfolio('Data/portfolio.csv')
-prices    = read_prices('Data/prices.csv')
+prices = read_prices('Data/prices.csv')
 
-# Calculate the total cost of the portfolio
-total_cost = 0.0
-for s in portfolio:
-    total_cost += s['shares']*s['price']
+# Generate the report data
 
-print(f'Total cost {total_cost}')
+report = make_report_data(portfolio, prices)
 
-# Compute the current value of the portfolio
-total_value = 0.0
-for s in portfolio:
-    total_value += s['shares']*prices[s['name']]
-
-print(f'Current value {total_value}')
-print(f'Gain {total_value - total_cost:0.2f}')
-
-
+# Output the report
+headers = ('Name', 'Shares', 'Price', 'Change')
+print('%10s %10s %10s %10s' % headers)
+print(('-' * 10 + ' ') * len(headers))
+for row in report:
+    print('%10s %10d %10.2f %10.2f' % row)
